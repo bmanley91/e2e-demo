3f2a574fae391e8653066d1f340603312f63a020
Brian Manley <brian.manley91@gmail.com>
2022-03-23 08:35:58 -0400

    Added Actual Dashboard Test (#2)
    * Add helper libs
    
    * Setup cypress support and finish dashboard test
    
    * Add instructions to readme.

--- cypress/integration/dashboard.test.js
+++ cypress/integration/dashboard.test.js
@@ -1,5 +1,21 @@
 describe ('Login to Dashboard', () => {
     it ('should greet the user with their username after logging in', () => {
-        cy.visit('/dashboard')
+        // Given a user exists
+        const username = 'person';
+        const password = 'abc123';
+
+        // When the user logs in to the dashboard page
+        cy.loginToPath({
+            username,
+            password,
+            path: '/dashboard'
+        });
+
+        // Then the user arrives on the dashboard
+        cy.url().should('include', '/dashboard')
+        cy.findByRole('heading', {name: /Dashboard/i}).should('exist');
+
+        // And the user's name is displayed in the greeting
+        cy.contains(`Hello ${username}`);
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,22 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { faker } from '@faker-js/faker'
+
+Cypress.Commands.add('getByTestId', (selector, ...args) => {
+    return cy.get(`[data-testid=${selector}]`, ...args)
+  })
+
+
+function loginToPath({
+    username = faker.name.firstName(),
+    password = faker.word.noun(),
+    path = '/'
+}) {
+    cy.visit(path);
+    cy.getByTestId('username-field')
+        .type(username);
+    cy.getByTestId(`password-field`)
+        .type(password);
+    cy.get('button[type=submit]').click()
+}
+  
+Cypress.Commands.add('loginToPath', loginToPath)
+  
